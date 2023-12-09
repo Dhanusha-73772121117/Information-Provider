5d84a575512e30d311e11d51462b781de69bb679
Dhanusha_73772121117 <kadhanusha@gmail.com>
2023-12-09 14:14:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> DHANUSHA'S CODE WORLD</h1>
+        <h2>PERSONAL INFO</h2>
+        <h3>Name:Dhanusha K A </h3>
+        <h3>Department:Information Technology </h3>
+        <h3>Register Number:73772121117</h3>
+        <h3>Year:BTECH-IT-III </h3>
+        <h3>Area of Interest:Full Stack Development </h3>
+        </header>
+        </div>
+  );
+}
+
+export default App;
